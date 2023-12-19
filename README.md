707e8be32ec45d84b5d73d2cd6558a2da13171f4
Harrison Schultz <hschultz1337@gmail.com>
2023-12-19 10:37:28 -0500

    Added identify script, updated README, added example rc

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # StoneSoupScripts
 Various scripts for Dungeon Crawl Stone Soup
+
+These scripts can be added to your rc file for Dungeon Crawl Stone Soup. They must be invoked manually during gameplay using macros. After adding a script to your rc file you must then create a macro to invoke it. Use at your own risk. If your character dies because of this script don't blame me.
+
+* Identify Scrolls and Potions
+This will automatically identify all scrolls and potions in the recommended way to do so. It works is as follows. (Keep in mind the script can only know what your character knows. So if you have not yet "identified" the scroll of identify then when you run the script you will have to manually choose something to identify. After your character knows the identify scroll this will happen automatically.)
+    - Potions are identified first unless you have no "scrolls of identify" (or have not discovered which scroll that is).
+    - Scrolls are NEVER identified with "scroll of identify". The script will ALWAYS use the scroll.
+    - IDENTIFIED scrolls and potions are NEVER used unless its a "scroll of identify".
+    - All scrolls/potions are used/identified in descending order starting with your highest quantity stack. 
+    - Unidentified potions will never be used automatically. Only unidentifed scrolls.
---
+++ identify-potions-and-scrolls.lua
@@ -0,0 +1,146 @@
+local infinite_loop_safety_net = 0
+local infinite_loop_safety_net_max = 100
+
+local auto_identify_scrolls_and_potions = false
+local waiting_for_scroll_effect = false
+local remaining_unidentified_scrolls_and_potions = false
+
+function ready()
+    if auto_identify_scrolls_and_potions and infinite_loop_safety_net < infinite_loop_safety_net_max then
+        infinite_loop_safety_net = infinite_loop_safety_net + 1
+
+        if not remaining_unidentified_scrolls_and_potions then
+            stop_identifying('Auto Identify Stopped - nothing left to identify.')
+        end
+
+        if waiting_for_scroll_effect then
+            take_action({'CMD_WAIT'})
+            waiting_for_scroll_effect = false
+        elseif remaining_unidentified_scrolls_and_potions then
+            identify_scrolls_and_potions()
+        end
+    end
+end
+
+function c_interrupt_activity(aname)
+    if string.find(aname, "rest") and auto_identify_scrolls_and_potions then
+        stop_identifying('Auto Identify Stopped - rest interrupted.')
+    end
+
+    return false
+end
+
+function identify_scrolls_and_potions() 
+    local item_list = items.inventory()
+    local scroll_unidentified_list = {}
+    local potion_unidentified_list = {}
+    local scroll_of_identify_letter = nil
+    local scroll_indexer = 0
+    local potion_indexer = 0
+
+    for _, item in pairs(item_list) do
+        if string.find(string.lower(item:name()), "scroll") then
+            if not item.fully_identified then
+                scroll_unidentified_list[scroll_indexer] = item
+                scroll_indexer = scroll_indexer + 1 
+            elseif string.find(item:name(), "identify") then
+                scroll_of_identify_letter = items.index_to_letter(item.slot)
+            end
+        end
+        if string.find(item:name(), "potion") then
+            if not item.fully_identified then
+                potion_unidentified_list[potion_indexer] = item
+                potion_indexer = potion_indexer + 1 
+            end
+        end
+    end
+
+    if table_length(scroll_unidentified_list) > 0 then
+        scroll_unidentified_list = sort_item_array(scroll_unidentified_list)
+    end
+    if table_length(potion_unidentified_list) > 0 then
+        potion_unidentified_list = sort_item_array(potion_unidentified_list)
+    end
+    
+    if table_length(scroll_unidentified_list) > 0 or (table_length(potion_unidentified_list) > 0 and scroll_of_identify_letter) then
+        remaining_unidentified_scrolls_and_potions = true 
+    else
+        remaining_unidentified_scrolls_and_potions = false
+    end
+
+    if scroll_of_identify_letter and table_length(potion_unidentified_list) > 0 then
+        identify_item(scroll_of_identify_letter, potion_unidentified_list[0])
+    elseif table_length(scroll_unidentified_list) > 0 then
+        read_scroll_and_rest(scroll_unidentified_list[0])
+    end
+end
+
+function stop_identifying(message)
+    if message then
+        notify_print(message)
+    end
+    remaining_unidentified_scrolls_and_potions = false
+    auto_identify_scrolls_and_potions = false
+    infinite_loop_safety_net = 0
+end
+
+function identify_item(identify_scroll_letter, item)
+    crawl.sendkeys("r"..identify_scroll_letter..items.index_to_letter(item.slot))
+end
+
+function read_scroll_and_rest(scroll)
+    waiting_for_scroll_effect = true
+    crawl.sendkeys("r"..items.index_to_letter(scroll.slot))
+end
+
+function sort_item_array(item_array)
+    local items_sorted = false
+    local iteration_count = 0
+    
+    while not items_sorted do
+        local last_item = nil
+        iteration_count = iteration_count + 1
+        items_sorted = true
+        for index = 0, table_length(item_array) - 1 do
+            local item = item_array[index]
+            if last_item then
+                if item.quantity > last_item.quantity then
+                    items_sorted = false
+                    item_array[index - 1] = item
+                    item_array[index] = last_item
+                else 
+                    item_array[index] = item
+                    last_item = item
+                end
+            else
+                item_array[index] = item
+                last_item = item
+            end
+        end
+    end
+
+    return item_array
+end
+
+function start_identify_scrolls_and_potions()
+    auto_identify_scrolls_and_potions = true
+    identify_scrolls_and_potions()
+end
+
+function table_length(T)
+    local count = 0
+    for _ in pairs(T) do count = count + 1 end
+    return count
+end
+
+function take_action(action, delay)
+    if delay then
+        crawl.delay(delay)
+    end
+    crawl.do_commands(action)
+end
+
+function notify_print(message)
+    crawl.mpr(message, notify_text_channel)
+end
+
